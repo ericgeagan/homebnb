7771c2c2c6effac33208caf5af433542bb964925
ericgeagan <ericgeagan@gmail.com>
2022-07-08 08:43:44 -0700

    home post route

--- app/api/home_routes.py
+++ app/api/home_routes.py
@@ -1,4 +1,57 @@
 from flask import Blueprint
 from flask_login import login_required
 from app.models import Home, db
-from app.forms import 
+from app.forms import HomeForm
+
+home_routes = Blueprint('homes', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+@home_routes.route('')
+def get_all_homes():
+	homes = Home.query.all()
+	return {"homes": [home.to_dict() for home in homes]}
+
+@home_routes.route('', methods=['POST'])
+@login_required
+def post_home():
+	#Add aws s3 upload
+	form = HomeForm()
+	form['csrf_token'].data = request.cookies['csrf_token']
+	if form.validate_on_submit():
+		new_home = Home(
+			user_id=form.data['user_id']
+			name=form.data['name']
+			address=form.data['address']
+			city=form.data['city']
+			state=form.data['state']
+			zipcode=form.data['zipcode']
+			bedrooms=form.data['bedrooms']
+			bathrooms=form.data['bathrooms']
+			beds=form.data['beds']
+			max_guests=form.data['max_guests']
+			description=form.data['description']
+			price=form.data['price']
+			tv=form.data['tv']
+			ac=form.data['ac']
+			wifi=form.data['wifi']
+			workspace=form.data['workspace']
+			kitchen=form.data['kitchen']
+			fridge=form.data['fridge']
+			microwave=form.data['microwave']
+			utensils=form.data['utensils']
+			grill=form.data['grill']
+			parking=form.data['parking']
+		)
+		db.session.add(new_home)
+		db.session.commit()
+		return new_home.to_dict()
+	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/home_form.py
+++ app/forms/home_form.py
@@ -1,3 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, DecimalField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, ValidationError
+
+def check_name(form, field):
+	name = field.data
+	if (len(name) > 200):
+		raise ValidationError("Name cannot be more than 200 characters")
+
+class HomeForm(FlaskForm):
+	user_id = IntegerField("user_id", validators=[DataRequired()])
+	name = StringField("name", validators=[DataRequired()])
+	address = StringField("address", validators=[DataRequired()])
+	city = StringField("city", validators=[DataRequired()])
+	state = StringField("state", validators=[DataRequired()])
+	zipcode = IntegerField("zipcode", validators=[DataRequired()])
+	bedrooms = IntegerField("bedrooms", validators=[DataRequired()])
+	bathrooms = IntegerField("bathrooms", validators=[DataRequired()])
+	beds = IntegerField("beds", validators=[DataRequired()])
+	max_guests = IntegerField("max_guests", validators=[DataRequired()])
+	description = TextAreaField("description")
+	price = DecimalField("price", validators=[DataRequired()])
+	tv = BooleanField("tv", validators=[DataRequired()])
+	ac = BooleanField("ac", validators=[DataRequired()])
+	wifi = BooleanField("wifi", validators=[DataRequired()])
+	workspace = BooleanField("workspace", validators=[DataRequired()])
+	kitchen = BooleanField("kitchen", validators=[DataRequired()])
+	fridge = BooleanField("fridge", validators=[DataRequired()])
+	microwave = BooleanField("microwave", validators=[DataRequired()])
+	utensils = BooleanField("utensils", validators=[DataRequired()]
+	grill = BooleanField("grill", , validators=[DataRequired()])
+	parking = BooleanField("parking", validators=[DataRequired()])
