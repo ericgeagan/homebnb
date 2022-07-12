094611a527b8d004af01a9d65da74b45f1bf0321
ericgeagan <ericgeagan@gmail.com>
2022-07-12 09:04:48 -0700

    conditional edit button render

--- react-app/src/components/HomePage/HomeCard.js
+++ react-app/src/components/HomePage/HomeCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { deleteHomeThunk } from '../../store/homes'
 import './homeCard.css'
 
 const HomeCard = ({home}) => {
+	const sessionUser = useSelector(state => state.session.user)
 	const dispatch = useDispatch()
 	const history = useHistory()
 
@@ -23,7 +24,7 @@ const HomeCard = ({home}) => {
 				<div>{home.name}</div>
 				<div>{home.address}</div>
 				<div>Wifi: {home.wifi ? 'Yes' : 'No'}</div>
-				<button onClick={() => handleEditButton()}>Edit</button>
+				{home?.user_id === sessionUser?.id ? <button onClick={() => handleEditButton()}>Edit</button> : <div></div> }
 				<button onClick={() => handleDelete()}>Delete</button>
 			</div>
 		</div>
