4010a94f79db5c6a71573c2ca2c72d617b483c8a
ericgeagan <ericgeagan@gmail.com>
2022-07-15 08:01:18 -0700

    fix

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,10 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, DateField
 from wtforms.validators import DataRequired, ValidationError
-
-def end_after_start(form, field):
 	
-
 class BookingForm(FlaskForm):
 		user_id = IntegerField("user_id", validators=[DataRequired()])
 		home_id = IntegerField("home_id", validators=[DataRequired()])
