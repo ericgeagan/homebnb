2369cd9a394e6be390e3ec5396d003114f08de91
Eric Geagan <ericgeagan@gmail.com>
2022-08-01 09:48:28 -0700

    fixed unspecific error mesages on home crud, and refactored error message check

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -14,12 +14,8 @@ def validation_errors_to_error_messages(validation_errors):
     errorMessages = []
     for field in validation_errors:
         for error in validation_errors[field]:
-            if field == 'email':
-                errorMessages.append(f'Please enter a valid email address.')
-            elif field == 'password':
-                errorMessages.append(f'Please enter a valid password.')
-            elif field == 'username':
-                errorMessages.append(f'Please enter a valid username.')
+            if error == 'This field is required.':
+                errorMessages.append(f'Please enter a valid {field}.')
             else:
                 errorMessages.append(f'{field}: {error}')
     return errorMessages
--- app/api/home_routes.py
+++ app/api/home_routes.py
@@ -12,8 +12,11 @@ def validation_errors_to_error_messages(validation_errors):
 	"""
 	errorMessages = []
 	for field in validation_errors:
-			for error in validation_errors[field]:
-					errorMessages.append(f'{error}')
+		for error in validation_errors[field]:
+			if error == 'This field is required.':
+				errorMessages.append(f'Please enter a valid {field}.')
+			else:
+				errorMessages.append(f'{field}: {error}')
 	return errorMessages
 
 @home_routes.route('')
